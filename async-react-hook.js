2ba4c0111dd06af6458f4c741be427d79dd8aa25
vvEii <coopershang125@gmail.com>
2020-12-13 10:58:02 -0800

    Add notes

--- async-react-hook.js
+++ async-react-hook.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default function AsyncHooks() {
+  const [search, setSearch] = useState('');
+
   return (
     <div>
       <h1>Async React Hooks</h1>
+      <form>
+        <input
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+          placeholder='Search for Gifs!'
+        />
+      </form>
     </div>
   )
 }
---
+++ notes.md
@@ -0,0 +1,4 @@
+## Gatsby vs CRA vs Next.js
+CRA is the client side rendering, Next.js is server side rendering, whereas Gatsby is static.
+
+If SEO is not needed, then choose CRA. If it needs SEO, and the data update very frequently, then choose use Next.js, such as Twitter, Reddit. 
